800561c73368dd2840bf1a75b941747f5af2f244
George Song <george@55minutes.com>
2013-06-04 11:49:13 -0700

    Install rbenv for the Jenkins user

--- provision.sh
+++ provision.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 # Update OS packages
-sudo aptitude update
-sudo DEBIAN_FRONTEND=noninteractive aptitude -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" safe-upgrade
+aptitude -q=2 -y update
+DEBIAN_FRONTEND=noninteractive aptitude -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" safe-upgrade
 
 # Install OS packages
-sudo add-apt-repository -y ppa:pitti/postgresql
-sudo aptitude -y install curl git-core jenkins libpq-dev nginx postgresql python-software-properties ufw virtualenvwrapper vim
+add-apt-repository -y ppa:pitti/postgresql
+aptitude -y install curl git-core jenkins libpq-dev nginx postgresql python-software-properties ufw virtualenvwrapper vim
 
 # Create PostgreSQL Jenkins user
-sudo su postgres -c "createuser jenkins --createdb --no-superuser --no-createrole"
+su -l postgres -c "createuser jenkins --createdb --no-superuser --no-createrole"
+
+# Install rbenv
+aptitude -y install git-core
+su -l jenkins -c "curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
+
+bashrc=$(cat <<'EOF'
+if [ -d $HOME/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
+EOF
+)
+echo "$bashrc" > /tmp/rbenvrc
+
+# Only replace ~/.bashrc if it doesn't already contain "rbenv init"
+su -l jenkins -c "grep -qs 'rbenv init' ~/.bashrc || cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp && mv ~/.bashrc.tmp ~/.bashrc"
