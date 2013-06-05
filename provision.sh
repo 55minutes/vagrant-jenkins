16b7babc741c3cebbb50c9b702e90948e6d2badc
George Song <george@55minutes.com>
2013-06-05 12:29:48 -0700

    Add more OS packages

--- provision.sh
+++ provision.sh
@@ -14,7 +14,7 @@ aptitude -q=2 -y update
 DEBIAN_FRONTEND=noninteractive aptitude -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" safe-upgrade
 
 ## Install OS packages
-aptitude -y install curl git-core imagemagick jenkins libmagickcore-dev libmagickwand-dev libpq-dev libqtwebkit-dev nginx nodejs postgresql python-software-properties ufw vim virtualenvwrapper
+aptitude -y install curl git-core imagemagick jenkins libicu48 libmagickcore-dev libmagickwand-dev libpq-dev libqtwebkit-dev nginx nodejs postgresql python-software-properties ufw vim virtualenvwrapper xvfb
 
 
 ####################
