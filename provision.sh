ac3f77fb7abff54fd3834f371e53e05ef7309c59
George Song <george@55minutes.com>
2013-06-04 20:56:07 -0700

    nginx configuration

--- provision.sh
+++ provision.sh
@@ -55,3 +55,33 @@ done
 
 ## Restart
 $jenkins_cli safe-restart
+
+
+## Nginx configuration
+jenkins=$(cat <<'EOF'
+upstream app_server {
+    server 127.0.0.1:8080 fail_timeout=0;
+}
+
+server {
+    listen 80;
+    listen [::]:80 default ipv6only=on;
+    server_name ci.55minutes.com;
+
+    location / {
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $http_host;
+        proxy_redirect off;
+
+        if (!-f $request_filename) {
+            proxy_pass http://app_server;
+            break;
+        }
+    }
+}
+EOF
+)
+echo "$jenkins" > /etc/nginx/sites-available/jenkins
+ln -s -f /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
+rm -f /etc/nginx/sites-enabled/default
+service nginx restart
