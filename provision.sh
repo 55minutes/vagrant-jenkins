254bb5862d9fc9bad763412742a85234a99e9267
George Song <george@55minutes.com>
2013-06-05 14:27:45 -0700

    Better proxy settings

--- provision.sh
+++ provision.sh
@@ -126,9 +126,11 @@ server {
     ignore_invalid_headers off;
 
     location / {
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header Host $http_host;
-        proxy_redirect off;
+        proxy_set_header X-Real-IP $remote_addr;  
+        proxy_set_header X-Forwarded-Proto https;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_redirect http:// https://;
 
         if (!-f $request_filename) {
             proxy_pass http://jenkins;
